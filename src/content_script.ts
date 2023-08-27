56869688067d0a7349a50bfaaa60f5a5a6ffedbe
Reorx <novoreorx@gmail.com>
2023-08-27 22:01:29 +0800

    WIP: support new repo page

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,32 +1,38 @@
 const className = 'toc-sidebar'
 
 class TocManager {
-  isOut: boolean
+  isShown: boolean
   el: Element
 
   constructor(el: Element) {
-    this.isOut = false
+    this.isShown = false
     this.el = el
   }
 
-  move(afterEl: Element, isOut: boolean) {
-    console.log('move', this.el, 'after', afterEl, isOut)
+  moveAfter(afterEl: Element, isShown: boolean) {
+    console.log('move', this.el, 'after', afterEl, isShown)
     afterEl.parentNode?.insertBefore(this.el, afterEl.nextSibling);
-    if (isOut) {
+    this.show(isShown)
+  }
+
+  moveIn(inEl: Element, isShown: boolean) {
+    console.log('move', this.el, 'in', inEl, isShown)
+    inEl.appendChild(this.el)
+    this.show(isShown)
+  }
+
+  show(isShown: boolean) {
+    if (isShown) {
       this.el.classList.add(className)
     } else {
       this.el.classList.remove(className)
     }
-    this.isOut = isOut
+    this.isShown = isShown
   }
 }
 
-function main() {
-  const elToc = document.querySelector('#readme details > details-menu')
-  if (!elToc) {
-    console.log('toc not found')
-    return
-  }
+function handleOldReadme(elToc: Element) {
+  console.log(`handle toc for "New Repository Overview" disabled`)
   /* Normal structure:
   <elDetailsContainer>
     <elDetails>
@@ -54,17 +60,86 @@ function main() {
 
   const onScroll = () => {
     const rect = elSidebarInnerLast!.getBoundingClientRect();
-    console.log('scroll', rect.top, rect.bottom)
-    if (rect.bottom < 0 && !tocManager.isOut) {
-      tocManager.move(elDetailsContainer, true)
-    } else if (rect.bottom > 0 && tocManager.isOut) {
-      tocManager.move(elTocButton, false)
+    // console.log('scroll', rect.top, rect.bottom)
+    if (rect.bottom < 0 && !tocManager.isShown) {
+      // show toc, move it besides elDetailsContainer
+      tocManager.moveAfter(elDetailsContainer, true)
+    } else if (rect.bottom > 0 && tocManager.isShown) {
+      // hide toc, move it besides elTocButton
+      tocManager.moveAfter(elTocButton, false)
     }
   }
 
   document.addEventListener('scroll', onScroll)
 
   onScroll()
+}
+
+function handleNewReadme() {
+  console.log(`handle toc for "New Repository Overview" enabled`)
+  /*
+  New Repository Overview uses react portal to control the toc position, the structure is like:
+
+  div#__primerPortalRoot__
+    div
+      div.Overlay__StyledOverlay-sc-51280t-0.jQOpaI
+        div
+          section(aria-labelledby="outline-id")
+  */
+  const elToc = document.querySelector('section[aria-labelledby="outline-id"]')
+  if (!elToc) {
+    console.warn('no toc found for "New Repository Overview" enabled')
+    return
+  }
+  const cloned = elToc.cloneNode(true) as Element
+  cloned.classList.add('toc-cloned')
+  cloned.removeAttribute('aria-labelledby')
+
+  const tocButton = document.querySelector('button[aria-label="Outline"]')
+  tocButton!.parentNode!.appendChild(cloned)
+
+  //const elTocContainer = elToc.parentElement!
+
+  const elSidebarInner = document.querySelector('.Layout-sidebar > div')!
+  const elSidebarInnerLast = elSidebarInner.lastElementChild!
+  console.log('inner last', elSidebarInnerLast)
+
+  const tocManager = new TocManager(cloned)
+
+
+  const onScroll = () => {
+    const rect = elSidebarInnerLast!.getBoundingClientRect();
+    // console.log('scroll', rect.top, rect.bottom)
+    if (rect.bottom < 0 && !tocManager.isShown) {
+      // show toc
+      tocManager.show(true)
+    } else if (rect.bottom > 0 && tocManager.isShown) {
+      // hide toc
+      tocManager.show(false)
+    }
+  }
+
+  document.addEventListener('scroll', onScroll)
+}
+
+function main() {
+  let elToc = document.querySelector('#readme details > details-menu')
+
+  if (elToc) {
+    handleOldReadme(elToc)
+  } else {
+
+    console.log('try for New Repository Overview enabled')
+    const tocButton = document.querySelector('button[aria-label="Outline"]')
+    if (!tocButton) {
+      console.warn('toc button not found, skip')
+      return
+    }
+    setTimeout(() => {
+      (tocButton as HTMLButtonElement).click()
+      setTimeout(handleNewReadme, 100)
+    }, 500)
+  }
 }
 
 main()
