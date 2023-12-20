7540d11948776317184de43e03976b465d9b86a5
Reorx <novoreorx@gmail.com>
2023-12-20 21:31:29 +0800

    remove old shits, now this extension are not relying on other existing ToC anymore

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,194 +1,21 @@
-const className = 'toc-sidebar'
+const tocClassName = 'toc-sidebar'
+const tocContentClassName = 'toc-sidebar-content'
+const stickyClassName = 'sticky-top'
 
-class TocManager {
-  isShown: boolean
-  el: HTMLElement
-  navEl: HTMLElement
-
-  constructor(el: HTMLElement) {
-    this.isShown = false
-    this.el = el
-    this.navEl = el.querySelector('nav') as HTMLElement
-  }
-
-  moveAfter(afterEl: HTMLElement, isShown: boolean) {
-    console.log('move', this.el, 'after', afterEl, isShown)
-    afterEl.parentNode?.insertBefore(this.el, afterEl.nextSibling);
-    this.show(isShown)
-  }
-
-  moveIn(inEl: HTMLElement, isShown: boolean) {
-    console.log('move', this.el, 'in', inEl, isShown)
-    inEl.appendChild(this.el)
-    this.show(isShown)
-  }
-
-  show(isShown: boolean) {
-    if (isShown) {
-      this.navEl.style.width = `${this.el.offsetWidth}px`
-      this.el.classList.add(className)
-    } else {
-      this.el.classList.remove(className)
-      this.navEl.style.width = 'auto'
-    }
-    this.isShown = isShown
-  }
-}
-
-function handleOldReadme(elToc: HTMLElement) {
-  console.log(`handle toc for "New Repository Overview" disabled`)
-  /* Normal structure:
-  <elDetailsContainer>
-    <elDetails>
-      <elTocButton/>
-      <elToc/>
-    </elDetails>
-  </elDetailsContainer>
-
-  Sidebar structure:
-  <elDetailsContainer>
-    <elDetails>
-      <elTocButton/>
-    </elDetails>
-  </elDetailsContainer>
-  <elToc/>
-  */
-  const elDetails = document.querySelector('#readme details')!
-  const elDetailsContainer = elDetails.parentElement!
-  const elTocButton = document.querySelector('#readme details > summary')! as HTMLElement
-
-  const elSidebarInner = document.querySelector('.Layout-sidebar > div')!
-  const elSidebarInnerLast = elSidebarInner.lastElementChild!
-
-  const tocManager = new TocManager(elToc)
-
-  const onScroll = () => {
-    const rect = elSidebarInnerLast!.getBoundingClientRect();
-    // console.log('scroll', rect.top, rect.bottom)
-    if (rect.bottom < 0 && !tocManager.isShown) {
-      // show toc, move it besides elDetailsContainer
-      tocManager.moveAfter(elDetailsContainer, true)
-    } else if (rect.bottom > 0 && tocManager.isShown) {
-      // hide toc, move it besides elTocButton
-      tocManager.moveAfter(elTocButton, false)
-    }
-  }
-
-  document.addEventListener('scroll', onScroll)
-
-  onScroll()
-}
-
-function handleNewReadme(tocButton: HTMLButtonElement) {
-  console.log(`handle toc for "New Repository Overview" enabled`)
-  /*
-  New Repository Overview uses react portal to control the toc position, the structure is like:
-
-  div#__primerPortalRoot__
-    div
-      div.Overlay__StyledOverlay-sc-51280t-0.jQOpaI
-        div
-          section(aria-labelledby="outline-id")
-  */
-  const elToc = document.querySelector('section[aria-labelledby="outline-id"]')
-  if (!elToc) {
-    console.warn('no toc found for "New Repository Overview" enabled')
-    return
-  }
-  // hide the original outline
-  tocButton.click()
-
-  const cloned = elToc.cloneNode(true) as HTMLElement
-  cloned.classList.add('toc-cloned')
-  cloned.removeAttribute('aria-labelledby')
-
-  const outlineHeading = document.createElement('h2');
-  outlineHeading.className = 'h4';
-  outlineHeading.textContent = 'Outline';
-  cloned.insertBefore(outlineHeading, cloned.children[1]);
-
-  //const tocButton = document.querySelector('button[aria-label="Outline"]')
-  //tocButton!.parentNode!.appendChild(cloned)
-
-  //const elTocContainer = elToc.parentElement!
-
-  const elSidebarInner = document.querySelector('.Layout-sidebar > div')!
-  elSidebarInner.appendChild(cloned)
-
-  const tocManager = new TocManager(cloned)
-
-
-  const onScroll = () => {
-    const rect = outlineHeading.getBoundingClientRect();
-    // console.log('scroll', rect.top, rect.bottom)
-    if (rect.bottom < 0 && !tocManager.isShown) {
-      // show toc
-      tocManager.show(true)
-    } else if (rect.bottom > 0 && tocManager.isShown) {
-      // hide toc
-      tocManager.show(false)
-    }
-  }
-
-  document.addEventListener('scroll', onScroll)
-}
-
-const addCSS = (css: string) => {
-  const styleElement: HTMLStyleElement = document.createElement('style');
-  document.head.appendChild(styleElement);
-  const sheet: CSSStyleSheet = styleElement.sheet as CSSStyleSheet;
-  sheet.insertRule(css, sheet.cssRules.length);
-};
-
-function main() {
-  // check if the url matches \/\w+/\w+$\
-  const projectPathRegex = /^\/[\w-]+\/[\w-]+\/?$/gm;
-  if (!projectPathRegex.exec(window.location.pathname)) {
-    console.log('not a project home path')
-    return
-  }
-
-
-  let elToc = document.querySelector('#readme details > details-menu') as HTMLElement
-
-  if (elToc) {
-    handleOldReadme(elToc)
-  } else {
-
-    console.log('New Repository Overview enabled!2')
-    const tocButton = document.querySelector('button[aria-label="Outline"]') as HTMLButtonElement
-    if (!tocButton) {
-      console.log('toc button not found, skip')
-      return
-    }
-    //addCSS('section[aria-labelledby="outline-id"] { display: none; }')
-    setTimeout(() => {
-      tocButton.click()
-      setTimeout(() => handleNewReadme(tocButton), 100)
-    }, 500)
-  }
-
-  const outline = createOutline()
-
-  const elSidebarInner = document.querySelector('.Layout-sidebar > div')!
-  elSidebarInner.appendChild(outline)
-}
-
-function createOutline() {
+function createToC() {
   const article = document.querySelector('article') as HTMLElement
   const headings = article.querySelectorAll('h1, h2, h3, h4, h5')
 
-  const outline = document.createElement('div')
-  outline.classList.add('toc-sidebar')
+  const toc = document.createElement('div')
   const ul = document.createElement('ul')
-  outline.appendChild(ul)
+  toc.appendChild(ul)
 
   const createLi = (text: string, href: string, headingTag: string) => {
     const li = document.createElement('li')
       const a = document.createElement('a')
       a.setAttribute('href', href)
         const label = document.createElement('div')
-        label.classList.add(`outline-label-${headingTag}`)
+        label.classList.add(`toc-label-${headingTag}`)
         label.innerText = text
         a.appendChild(label)
       li.appendChild(a)
@@ -203,7 +30,63 @@ function createOutline() {
     const href = a.getAttribute('href')!
     createLi((h.textContent || '').trim(), href, h.tagName.toLowerCase())
   }
-  return outline
+  return toc
+}
+
+function main() {
+  // check if the url matches \/\w+/\w+$\
+  const projectPathRegex = /^\/[\w-]+\/[\w-]+\/?$/gm;
+  if (!projectPathRegex.exec(window.location.pathname)) {
+    console.log('not a project home path')
+    return
+  }
+
+  // create section that will be added to sidebar later
+  const section = document.createElement('section')
+  section.classList.add(tocClassName)
+
+  // create title for section
+  const title = document.createElement('h2');
+  title.className = 'h4';
+  title.textContent = 'Outline';
+  section.appendChild(title)
+
+  // create toc
+  const toc = createToC()
+  toc.classList.add(tocContentClassName)
+  section.appendChild(toc)
+
+  // add section to sidebar
+  const elSidebarInner = document.querySelector('.Layout-sidebar > div')!
+  elSidebarInner.appendChild(section)
+
+  let isSticky = false
+  const toggleSticky = (flag: boolean) => {
+    if (flag) {
+      toc.style.width = `${toc.offsetWidth}px`
+      toc.classList.add(stickyClassName)
+    } else {
+      toc.classList.remove(stickyClassName)
+      toc.style.width = 'auto'
+    }
+    isSticky = flag
+  }
+
+  // handle scroll
+  const onScroll = () => {
+    /* make toc sticky to top when scrolled by */
+    const rect = title.getBoundingClientRect();
+    // console.log('scroll', rect.top, rect.bottom)
+    if (rect.bottom < 0 && !isSticky) {
+      // sticky toc
+      toggleSticky(true)
+    } else if (rect.bottom > 0 && isSticky) {
+      // unsticky toc
+      toggleSticky(false)
+    }
+  }
+
+  document.addEventListener('scroll', onScroll)
 }
 
 main()
